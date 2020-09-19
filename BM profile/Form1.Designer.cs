ba55da0b60283eef78dceb44cc5bfc3c1e79ec27
Khanh Trần <40160447+KhanhTran0112@users.noreply.github.com>
2020-09-19 22:58:10 +0700

    a
    a

---
+++ BM profile/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BM_profile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLinkBM = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Link BM";
+            // 
+            // txtLinkBM
+            // 
+            this.txtLinkBM.Location = new System.Drawing.Point(12, 25);
+            this.txtLinkBM.Multiline = true;
+            this.txtLinkBM.Name = "txtLinkBM";
+            this.txtLinkBM.Size = new System.Drawing.Size(174, 413);
+            this.txtLinkBM.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Chuyển giao BM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(192, 25);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(100, 20);
+            this.txtpassword.TabIndex = 4;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(192, 51);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Mở chrome";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLinkBM);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLinkBM;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ BM profile/Form1.cs
@@ -0,0 +1,117 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BM_profile
+{
+    public partial class Form1 : Form
+    {
+
+        string ProfileFolderPath = "Profile";
+        ChromeDriver driver;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (driver != null)
+            {
+                try
+                {
+                    driver.Close();
+                    driver.Quit();
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < txtLinkBM.Lines.Length; i++)
+            {
+                driver.Url = txtLinkBM.Lines[i];
+                driver.Navigate();
+                Thread.Sleep(3000);
+                try
+                {
+                    var name = driver.FindElementByXPath("//*[@id=\"js_0\"]");
+                    name.SendKeys("Khanh Tran");
+                    Thread.Sleep(3000);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Không tìm thấy ô để nhập tên " + txtLinkBM.Lines[i]);
+                }
+
+
+                try
+                {
+                    var continues = driver.FindElementByXPath("/html/body/div/div/div[3]/div/div/form/div[2]/div[2]/button");
+                    continues.Click();
+                    Thread.Sleep(3000);
+                }
+                catch
+                {
+                    MessageBox.Show("Không tìm thấy nut bam de tiep tuc " + txtLinkBM.Lines[i]);
+                }
+
+                try
+                {
+                    var password = driver.FindElementByXPath("/html/body/div[1]/div/div[3]/div/div/form/div[3]/div[1]/div/div[2]/div[3]/div/div/span/input");
+                    password.SendKeys(txtpassword.Text);
+                    Thread.Sleep(3000);
+                }
+                catch
+                {
+                    MessageBox.Show("Không tìm thấy o để nhập mật khẩu " + txtLinkBM.Lines[i]);
+                }
+
+                try
+                {
+                    var verification = driver.FindElementByXPath("/html/body/div[1]/div/div[3]/div/div/form/div[3]/div[2]/button");
+                    verification.Click();
+                    Thread.Sleep(10000);
+                }
+                catch
+                {
+                    MessageBox.Show("Không tìm thấy nút xác thực " + txtLinkBM.Lines[i]);
+                }
+                Console.WriteLine("Đã xong:  " + txtLinkBM.Lines[i]);
+            }
+            MessageBox.Show("Đã hoàn thành chuyển giao tài khoản");
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            ChromeOptions options = new ChromeOptions();
+
+            if (!Directory.Exists(ProfileFolderPath))
+            {
+                Directory.CreateDirectory(ProfileFolderPath);
+            }
+
+            if (Directory.Exists(ProfileFolderPath))
+            {
+                int nameCount = 0;
+
+                options.AddArguments("user-data-dir=" + ProfileFolderPath + "\\" + nameCount);
+            }
+            driver = new ChromeDriver(options);
+            driver.Url = "https://www.facebook.com/";
+            driver.Navigate();
+        }
+    }
+}
